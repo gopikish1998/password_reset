b46906b1717331f49d39bf78118b6573391e8cb1
Gopi Kishore <kishorepenjendra@gmail.com>
2021-12-26 23:43:21 +0530

    backend site added

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,7 @@
 let getSettings = () => {
     return {
-        api : "http://localhost:4000"
-        // api : "https://recipe-management-gopi.herokuapp.com"
+        // api : "http://localhost:4000"
+        api : "password-reset-gopi.herokuapp.com"
     }
 }
 
